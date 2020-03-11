7cfa7f66d69a161a7273c534fe237b3d43bd1ddf
brian <riceb53@gmail.com>
2020-03-10 20:33:12 -0400

    add migration with default: true

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_10_231413) do
+ActiveRecord::Schema.define(version: 2020_03_11_003023) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2020_03_10_231413) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.decimal "price", precision: 6, scale: 2
+    t.integer "stock", default: 0
   end
 
 end
